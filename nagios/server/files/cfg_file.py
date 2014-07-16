c8742f46f4568da5c821c399582b3528ee4db470
Robert Fairburn <robert.fairburn@gmail.com>
2014-07-16 14:54:35 +0000

    Initial commit

---
+++ nagios/server/files/cfg_file.py
@@ -0,0 +1,14 @@
+def run():
+  output = ""
+  for type,defines in context['configs'].items():
+    output += "# " + type + "\n\n"
+    for name,define in defines.items():
+      output += "define " + type + "{\n"
+      length = 0
+      for item,value in define.items():
+        if len(item) > length:
+          length = len(item)
+      for item,value in define.items():
+        output += "    " + item.ljust(length) + " " + value + "\n"
+      output += "}\n\n"
+  return output
