3073d81f568f21dab626f3f7b07215c1c2f262c4
Robert Fairburn <robertfairburn@Roberts-MacBook-Pro.local>
2014-07-21 11:19:41 -0500

    another test

--- nagios/server/process_autoconfig.py
+++ nagios/server/process_autoconfig.py
@@ -1,10 +1,10 @@
-#!py
+#!pyobjects
 
-def run():
-  config = {}
-  global process_autoconfig = False
-  for minion_id, minion_grains in __salt__['mine.get']('*', grains.items).items():
-    if ('nagios' in minion_grains['roles']) or ('nagios.nrpe' in minion_grains['roles']):
-      process_autoconfig = True
-  config = {'process_autoconfig': process_autoconfig}
-  return config
+value_process_autoconfig = False
+for minion_id, minion_grains in __salt__['mine.get']('*', grains.items).items():
+  if ('nagios' in minion_grains['roles']) or ('nagios.nrpe' in minion_grains['roles']):
+    value_process_autoconfig = True
+
+class process_autoconfig(Map):
+  process_autoconfig = value_process_autoconfig
+
