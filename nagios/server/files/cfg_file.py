cd38ef871e619aa5444fb31528cb61293628848c
Robert Fairburn <robertfairburn@Roberts-MacBook-Pro.local>
2014-07-21 15:54:53 -0500

    alter config layouts to match preferenes John is currently using

--- nagios/server/files/cfg_file.py
+++ nagios/server/files/cfg_file.py
@@ -3,11 +3,11 @@ def run():
   for type,defines in context['configs'].items():
     output += "# " + type + "\n\n"
     for name,define in defines.items():
-      output += "define " + type + "{\n"
+      output += "define " + type + " {\n"
       length = 0
       for item,value in define.items():
-        if len(item) > length:
-          length = len(item)
+        if len(item) >= length:
+          length = len(item) + 1
       for item,value in define.items():
         output += "    " + item.ljust(length) + " " + value + "\n"
       output += "}\n\n"
