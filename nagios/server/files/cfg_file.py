3b1571294d53e0c642727345acb3c1d64b205e5b
Robert Fairburn <robertfairburn@Roberts-MacBook-Pro.local>
2014-07-23 12:29:10 -0500

    attempt to bring order to defines

--- nagios/server/files/cfg_file.py
+++ nagios/server/files/cfg_file.py
@@ -2,13 +2,15 @@ def run():
   output = ""
   for type,defines in context['configs'].items():
     output += "# " + type + "\n\n"
-    for name,define in defines.items():
+    for name,define_list in defines.items():
       output += "define " + type + " {\n"
       length = 0
-      for item,value in define.items():
+      for define in define_list:
+        item,value = define.items()[0]
         if len(item) >= length:
           length = len(item) + 1
-      for item,value in define.items():
+      for define in define_list:
+        item,value = define.items()[0]
         output += "    " + item.ljust(length) + " " + value + "\n"
       output += "}\n\n"
   return output
